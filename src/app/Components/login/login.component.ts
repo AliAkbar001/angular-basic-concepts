55e9d5a7427a09d415c315ed43db110db98f7935
AliAkbar001 <webtechsolution786786@gmail.com>
2022-07-10 20:10:02 +0500

    form-validation

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  username : string = ""
+  password: string = ""
+  errorMsg: string = ""
   constructor() { }
 
   ngOnInit(): void {
   }
-
+login(){
+  if(this.username.trim().length === 0){
+    this.errorMsg = "Username is required"
+  }else if(this.password.trim().length === 0){
+    this.errorMsg = "Password is required"
+  }else if(this.username !== "admin" || this.password !== "admin"){
+  this.errorMsg = "Invalid username or password"
+  }else{
+    this.errorMsg = ""
+    alert("Login Successfully")
+  }
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './Components/login/login.component';
 import { HomeComponent } from './Components/home/home.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { MovieComponent } from './Components/movie/movie.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { MovieComponent } from './Components/movie/movie.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
