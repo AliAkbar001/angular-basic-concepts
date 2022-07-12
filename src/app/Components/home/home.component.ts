1d6cd497453a6bac26a469833b2ec1e2c670e296
AliAkbar001 <webtechsolution786786@gmail.com>
2022-07-12 23:42:42 +0500

    set-apis

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -9,10 +9,24 @@ import { HttpClient } from '@angular/common/http';
 export class HomeComponent implements OnInit {
 
   trendingMovies : any;
+  theatreMovies : any;
+  popularMovies : any;
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getTrendingMovies();
+    this.getTheaterMovies();
+    this.getPopularMovies();
+  }
+  getPopularMovies(){
+    this.http.get("http://localhost:4200/assets/data/popular-movies.json").subscribe((movies)=>{
+      this.popularMovies = movies
+    })
+  }
+  getTheaterMovies(){
+    this.http.get("http://localhost:4200/assets/data/theatre-movies.json").subscribe((movies)=>{
+      this.theatreMovies = movies
+    })
   }
   getTrendingMovies(){
     this.http.get("http://localhost:4200/assets/data/trending-movies.json").subscribe((movies)=>{
